837ef0d25e136c24d7908f70d6ae99b9df70ab50
Elodie Gauthier <elodie.gauthier@imag.fr>
2017-02-07 11:53:50 +0100

    Add lines to use the length-contrasted acoustic dictionnary

--- kaldi-scripts/02_lexicon.sh
+++ kaldi-scripts/02_lexicon.sh
@@ -11,6 +11,7 @@ pushd data/local/dict
 
 ##create nonsilence_phones.txt
 cat ../lang/lexicon.txt | awk '{for (i=2;i<=NF;i++) print $i}' | sort -u > nonsilence_phones.txt
+#cat ../lang_lengthLabel/lexicon.txt | awk '{for (i=2;i<=NF;i++) print $i}' | sort -u > nonsilence_phones.txt    #--2 phone units for 1 vowel (i.e.: vowel /a/ is represented [a] and [aL])
 
 ##create silence_phones.txt
 echo "SIL" > silence_phones.txt
@@ -20,9 +21,11 @@ touch extra_questions.txt
 rm -f lexicon.txt # if run twice, don't append to old one
 ##lexicon.txt
 cat ../lang/lexicon.txt | sed 's/(.)//' > lexicon.txt
+#cat ../lang_lengthLabel/lexicon.txt | sed 's/(.)//' > lexicon.txt   #-- vowels are length contrasted in the acoustic dictionary 
 
-#write UNK symbol
+##write UNK symbol
 echo -e "SIL\tSIL" >> lexicon.txt
 echo -e "<UNK>\tSIL" >> lexicon.txt
 echo "SIL" > optional_silence.txt
 echo "<UNK>" > ../lang/oov.txt
+#echo "<UNK>" > ../lang_lengthLabel/oov.txt
